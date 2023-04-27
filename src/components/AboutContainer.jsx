c5ba93f86a77bdf17367c0977fa2cead7d4e8e7a
Hugo Silva <hugo.silva@indexdigital.com.br>
2023-04-27 13:38:52 -0300

    Adicionar página principal

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,21 @@
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi enim
+        ea optio numquam ut culpa, doloribus nostrum perspiciatis pariatur
+        aliquam architecto, amet magni cupiditate odit tempore ratione
+        necessitatibus dolor fugiat!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi,
+        molestias veritatis! Voluptatibus, omnis sapiente! Tenetur quam, quis
+        soluta vero harum aliquid sequi, non nisi, porro corporis deleniti
+        consequatur id a.
+      </p>
+    </section>
+  );
+};
+
+export default AboutContainer;
---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,17 @@
+const ProjectsContainer = () => {
+  return (
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia unde quae
+        obcaecati totam? Minima, quo veritatis corrupti nostrum tenetur
+        molestiae sequi dolores aliquam. Ex amet obcaecati quis ad iusto esse!
+      </p>
+      <a href="#" className="btn">
+        Ver Projetos
+      </a>
+    </section>
+  );
+};
+
+export default ProjectsContainer;
